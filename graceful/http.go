d4009c44ae4d9814d7371b9fad7014450feb84cb
NicatorBa <daniel@bartschinski.info>
2026-06-02 22:07:20 +0200

    Update to runner and initializer implementation

--- graceful/http.go
+++ graceful/http.go
@@ -10,57 +10,68 @@ import (
 )
 
 type (
-	HttpListenAndServeOptions struct {
+	ServerOptions struct {
 		Addr            string
 		ShutdownTimeout time.Duration
 	}
 
-	HttpListenAndServeOpt func(*HttpListenAndServeOptions) error
+	ServerOptionFunc func(*ServerOptions) error
 )
 
-func HttpListenAndServe(handler http.Handler, opts ...HttpListenAndServeOpt) RunnerFunc {
-	options := HttpListenAndServeOptions{
+type serverRunner struct {
+	handler http.Handler
+	opts    []ServerOptionFunc
+	options ServerOptions
+}
+
+func (s *serverRunner) Initialize() error {
+	s.options = ServerOptions{
 		ShutdownTimeout: 5 * time.Second,
 	}
-	for _, opt := range opts {
-		opt(&options)
+	for _, opt := range s.opts {
+		if err := opt(&s.options); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *serverRunner) Run(ctx context.Context) {
+	logger := logging.From(ctx)
+	srv := &http.Server{
+		Addr:    s.options.Addr,
+		Handler: s.handler,
 	}
 
-	return func(ctx context.Context) {
-		logger := logging.From(ctx)
-		srv := &http.Server{
-			Addr:    options.Addr,
-			Handler: handler,
+	go func() {
+		if err := srv.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			logger.Error().Err(err).Msg("HTTP server error")
 		}
+	}()
 
-		go func() {
-			err := srv.ListenAndServe()
-			if err != nil {
-				logger.Error().Err(err).Msg("HTTP server error")
-			}
-		}()
+	<-ctx.Done()
 
-		<-ctx.Done()
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), s.options.ShutdownTimeout)
+	defer cancel()
 
-		shutdownCtx, cancel := context.WithTimeout(context.Background(), options.ShutdownTimeout)
-		defer cancel()
-
-		err := srv.Shutdown(shutdownCtx)
-		if err != nil {
-			logger.Error().Err(err).Msg("HTTP server shutdown failed")
-		}
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		logger.Error().Err(err).Msg("HTTP server shutdown failed")
 	}
 }
 
-func WithAddr(addr string) HttpListenAndServeOpt {
-	return func(opts *HttpListenAndServeOptions) error {
+func ListenAndServe(handler http.Handler, opts ...ServerOptionFunc) Runner {
+	return &serverRunner{handler: handler, opts: opts}
+}
+
+func WithAddr(addr string) ServerOptionFunc {
+	return func(opts *ServerOptions) error {
 		opts.Addr = addr
 		return nil
 	}
 }
 
-func WithShutdownTimeout(timeout time.Duration) HttpListenAndServeOpt {
-	return func(opts *HttpListenAndServeOptions) error {
+func WithShutdownTimeout(timeout time.Duration) ServerOptionFunc {
+	return func(opts *ServerOptions) error {
 		if timeout <= 0 {
 			return errors.New("timeout value must be greater than 0")
 		}
